dd38e0f99978145620c01f9c1e82f521b2519263
AryanMore <aryanmore498@gmail.com>
2023-03-01 17:01:21 +0530

    Adding Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavbarDiv from './components/navbar/Navbar.component';
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <NavbarDiv />
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.component.jsx
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import './navbar.styles.css';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+const NavbarDiv = () => {
+    return (
+        <Fragment>
+            <Navbar className='navbar-div'>
+                <Container>
+                    <Navbar.Brand href="/"><i><b>moreNews</b></i></Navbar.Brand>
+                    <NavDropdown title="Quick Route" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">
+                            Another action
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                            Something else here
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                </Container>
+            </Navbar>
+        </Fragment>
+    )
+}
+
+export default NavbarDiv;
